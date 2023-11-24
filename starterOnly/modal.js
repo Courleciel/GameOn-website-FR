4e6a6d165591e255964480985e9493f4d5f57430
luc perrot <luc.perrot@hotmail.fr>
2023-11-24 13:13:22 +0100

    feature new error messages

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -34,52 +34,79 @@ document.forms["reserve"].addEventListener("submit", function(e) {
 
   // Validation du prénom
   var firstName = document.getElementById("first").value;
+  var firstNameError = document.getElementById("first-name-error");
   if(firstName.length < 2) {
-    alert("Le prénom doit contenir au moins 2 caractères.");
+    firstNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
     isValid = false;
+  } else {
+    firstNameError.textContent = "";
   }
 
   // Validation du nom
   var lastName = document.getElementById("last").value;
+  var lastNameError = document.getElementById("last-name-error");
   if(lastName.length < 2) {
-    alert("Le nom doit contenir au moins 2 caractères.");
+    lastNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
     isValid = false;
+  } else {
+    lastNameError.textContent = "";
   }
 
   // Validation de l'email
   var email = document.getElementById("email").value;
+  var emailError = document.getElementById("email-error");
   var emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if(!emailRegEx.test(email)) {
-    alert("Veuillez entrer une adresse email valide.");
+    emailError.textContent = "Veuillez entrer une adresse email valide.";
     isValid = false;
+  } else {
+    emailError.textContent = "";
+  }
+
+  var birthdate = document.getElementById("birthdate").value;
+  var birthdateError = document.getElementById("birthdate-error");
+  if (!birthdate) {
+    birthdateError.textContent = "Vous devez entrer votre date de naissance.";
+    isValid = false;
+  } else {
+    birthdateError.textContent = "";
   }
 
   // Validation du nombre de concours
   var quantity = document.getElementById("quantity").value;
-  if(isNaN(quantity) || quantity === "") {
-    alert("Veuillez entrer un nombre valide pour le nombre de concours.");
+  var quantityError = document.getElementById("quantity-error");
+  if (isNaN(quantity) || quantity === "") {
+    quantityError.textContent = "Veuillez entrer un nombre valide pour le nombre de tournois.";
     isValid = false;
+  } else {
+    quantityError.textContent = "";
   }
 
   // Validation du choix d'un bouton radio
   var locations = document.forms["reserve"]["location"];
+  var locationError = document.getElementById("location-error");
   var locationSelected = false;
-  for(var i = 0; i < locations.length; i++) {
-    if(locations[i].checked) {
+  for (var i = 0; i < locations.length; i++) {
+    if (locations[i].checked) {
       locationSelected = true;
       break;
     }
   }
-  if(!locationSelected) {
-    alert("Veuillez sélectionner un lieu pour le tournoi.");
+  if (!locationSelected) {
+    locationError.textContent = "Vous devez choisir une option.";
     isValid = false;
+  } else {
+    locationError.textContent = "";
   }
 
   // Validation de la case des conditions générales
   var termsChecked = document.getElementById("checkbox1").checked;
-  if(!termsChecked) {
-    alert("Vous devez accepter les conditions d'utilisation.");
+  var termsError = document.getElementById("terms-error");
+  if (!termsChecked) {
+    termsError.textContent = "Vous devez vérifier que vous acceptez les termes et conditions.";
     isValid = false;
+  } else {
+    termsError.textContent = "";
   }
 
   // Empêcher l'envoi du formulaire si la validation échoue
