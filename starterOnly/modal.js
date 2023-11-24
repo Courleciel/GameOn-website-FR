2e9c13670a678e0a924a710ec2d84bab21dd232d
luc perrot <luc.perrot@hotmail.fr>
2023-11-24 16:40:43 +0100

    style red border when errors form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -36,51 +36,66 @@ document.forms["reserve"].addEventListener("submit", function(e) {
   // Validation du prénom
   var firstName = document.getElementById("first").value;
   var firstNameError = document.getElementById("first-name-error");
+  var firstNameFormData = document.querySelector('.formData#first-form-data');
   if(firstName.length < 2) {
     firstNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+    firstNameFormData.setAttribute('data-error-visible', 'true');
     isValid = false;
   } else {
     firstNameError.textContent = "";
+    firstNameFormData.setAttribute('data-error-visible', 'false');
   }
 
   // Validation du nom
   var lastName = document.getElementById("last").value;
   var lastNameError = document.getElementById("last-name-error");
+  var lastNameFormData = document.querySelector('.formData#last-form-data');
   if(lastName.length < 2) {
     lastNameError.textContent = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
+    lastNameFormData.setAttribute('data-error-visible', 'true');
     isValid = false;
   } else {
     lastNameError.textContent = "";
+    lastNameFormData.setAttribute('data-error-visible', 'false');
   }
 
   // Validation de l'email
   var email = document.getElementById("email").value;
   var emailError = document.getElementById("email-error");
   var emailRegEx = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  var emailFormData = document.querySelector('.formData#email-form-data');
   if(!emailRegEx.test(email)) {
     emailError.textContent = "Veuillez entrer une adresse email valide.";
+    emailFormData.setAttribute('data-error-visible', 'true');
     isValid = false;
   } else {
     emailError.textContent = "";
+    emailFormData.setAttribute('data-error-visible', 'false');
   }
 
   var birthdate = document.getElementById("birthdate").value;
   var birthdateError = document.getElementById("birthdate-error");
+  var birthdateFormData = document.querySelector('.formData#birthdate-form-data');
   if (!birthdate) {
     birthdateError.textContent = "Vous devez entrer votre date de naissance.";
+    birthdateFormData.setAttribute('data-error-visible', 'true');
     isValid = false;
   } else {
     birthdateError.textContent = "";
+    birthdateFormData.setAttribute('data-error-visible', 'false');
   }
 
   // Validation du nombre de concours
   var quantity = document.getElementById("quantity").value;
   var quantityError = document.getElementById("quantity-error");
+  var quantityFormData = document.querySelector('.formData#quantity-form-data');
   if (isNaN(quantity) || quantity === "") {
     quantityError.textContent = "Veuillez entrer un nombre valide pour le nombre de tournois.";
+    quantityFormData.setAttribute('data-error-visible', 'true');
     isValid = false;
   } else {
     quantityError.textContent = "";
+    quantityFormData.setAttribute('data-error-visible', 'false');
   }
 
   // Validation du choix d'un bouton radio
@@ -127,7 +142,7 @@ document.forms["reserve"].addEventListener("submit", function(e) {
     closeButton.style.display = "block";
 
     // Gérer le clic sur le bouton "Fermer"
-    
+
     closeButton.onclick = function() {
     modalbg.style.display = "none";
     };
